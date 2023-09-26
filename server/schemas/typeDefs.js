1a0051e78e0ec562e8be8c81c76b1edd6b08b0f2
Cameron French <cameron.french92@gmail.com>
2023-09-25 21:19:09 -0400

    added mutations for saving and removing books to lists for profiles pages

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -8,6 +8,15 @@ type User {
   password: String
 }
 
+type Book {
+    bookId: ID!
+    authors: String
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
 type Auth {
   token: ID!
   user: User
@@ -28,7 +37,12 @@ type Query {
 type Mutation {
   addUser(email:String!, username:String!, password:String!): Auth
   login(email:String!, password:String!): Auth
+
   addUserPreference(userId: String!, favoriteGenre: String!): UserPreferences
+  saveBook(profileId: ID!, book: String!): User
+  addToRead(profileId: ID!, book: String!): User
+
+  removeBook(book: String!): User
 }
 `;
 
