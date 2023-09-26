86d40521ec569b3b764298bd52f2d91c63572884
zoe <zoeedge0703@gmail.com>
2023-09-25 19:03:56 -0500

    created the backend for the choosing fav genres page

---
+++ server/models/UserPreferences.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+const User = require('./User');
+
+
+const userPreferencesSchema = new Schema ({
+    // should I make this unique or not unique?
+    _id: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    favoriteGenres: {
+        type: String,
+        requires: true
+    }
+});
+
+const UserPreferences = model('UserPreferences', userPreferencesSchema);
+
+module.exports = UserPreferences;
