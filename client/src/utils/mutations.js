fbd307c640c31c185acce8321d0c01fafffb5abe
Cameron French <cameron.french92@gmail.com>
2023-09-28 22:22:12 -0400

    added readBooks list to Profile and added necessary mutations for data retrieval

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -12,6 +12,54 @@ export const LOGIN_USER = gql`
   }
 `;
 
+export const SAVE_BOOK = gql`
+  mutation saveBook($authors: [String], $description: String!, $bookId: String!, $image: String, $title: String!, $userId: ID!) {
+    saveBook(authors: $authors, description: $description, bookId: $bookId, image: $image, title: $title, userId: $userId) {
+      _id
+      username
+      savedBooks {
+        authors
+        description
+        bookId
+        image
+        title
+      }
+    }
+  }
+`;
+
+export const READ_BOOK = gql`
+  mutation readBook($authors: [String], $description: String!, $bookId: String!, $image: String, $title: String!, $userId: ID!) {
+    readBook(authors: $authors, description: $description, bookId: $bookId, image: $image, title: $title, userId: $userId) {
+      _id
+      username
+      readBooks {
+        authors
+        description
+        bookId
+        image
+        title
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      savedBooks {
+        authors
+        description
+        bookId
+        image
+        title
+      }
+    }
+  }
+`;
+
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
@@ -20,26 +68,6 @@ export const ADD_USER = gql`
         _id
         username
       }
-    }
-  }
-`;
-
-export const SAVE_BOOK = gql`
-  mutation addBook($userId: ID!, $book: String!) {
-    addBook(userId: $userId, book: $book) {
-      _id
-      name
-      books
-    }
-  }
-`;
-
-export const REMOVE_BOOK = gql`
-  mutation removeBook($book: String!) {
-    removeBook(book: $book) {
-      _id
-      name
-      books
     }
   }
 `;
