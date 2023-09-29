90f0962b4c2ecb572abc6c7c16f1330afe62dcde
Cameron French <cameron.french92@gmail.com>
2023-09-28 21:22:11 -0400

    added Want To Read container to profile page that will display saved books that the user will want to read. Also added necessary mutations for functionality for Want To Read List

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -23,3 +23,23 @@ export const ADD_USER = gql`
     }
   }
 `;
+
+export const SAVE_BOOK = gql`
+  mutation addBook($userId: ID!, $book: String!) {
+    addBook(userId: $userId, book: $book) {
+      _id
+      name
+      books
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($book: String!) {
+    removeBook(book: $book) {
+      _id
+      name
+      books
+    }
+  }
+`;
